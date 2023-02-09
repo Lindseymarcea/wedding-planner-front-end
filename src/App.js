75cdbf8c5435e65d1192311589d2fabc75070e85
Lindsey <lindseymarcea@icloud.com>
2023-02-09 15:26:43 -0500

    added router links to all pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import TaskList Page from './pages/taskListPage';
 import Cuisine from './pages/cuisinePage';
 import HomePage from './pages/homePage';
+import GiftRegistry from './pages/giftRegistryPage';
+import Guestlist from './pages/guestlistPage';
+import Photo from './pages/photoPage';
+import TaskList from './pages/taskListPage';
+import VisionBoard from './pages/visionBoardPage';
+import Playlist from './pages/playlistPage';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 // class App extends cuisine {
@@ -58,10 +64,10 @@ export default class App extends React.Component {
         <header className='App-header'>
           <nav>
             <h1> A & J</h1>
-            <navLink to='/'>Home</navLink>
+            {/* <navLink to='/'>Home</navLink> */}
             <h2> Our Wedding Story </h2>
             <h3> Aaliyah & James</h3>
-            <navLink to='RSVP'>RSVP</navLink>
+            {/* <navLink to='RSVP'>RSVP</navLink> */}
             <button>RSVP</button>
             {/* <Link to='/cuisine'>Cuisine Selection</Link> */}
             <button>Gift Registry</button>
@@ -75,27 +81,25 @@ export default class App extends React.Component {
           <Routes>
             <Route exact path='/' element={<HomePage />} />
             <Route exact path='cuisine' element={<Cuisine />} />
+            <Route exact path='giftregistry' element={<GiftRegistry />} />
+            <Route exact path='guestlist' element={<Guestlist />} />
+            <Route exact path='photo' element={<Photo />} />
+            <Route exact path='playlist' element={<Playlist />} />
+            <Route exact path='tasklist' element={<TaskList />} />
+            <Route exact path='visionboard' element={<VisionBoard />} />
           </Routes>
           <Link to='/cuisine'>Cuisine Selection</Link>
+          <Link to='/giftregistry'>Gift Registry</Link>
+          <Link to='/guestlist'>RSVP</Link>
+          <Link to='/photo'>Upload Memories</Link>
+          <Link to='/playlist'>Playlist</Link>
+          <Link to='/tasklist'>To Do</Link>
+          <Link to='/visionboard'>Vision Board</Link>
         </Router>
       </>
     );
   }
 }
-
-// export default App;
-
-// line 5 edited & 15-49
-
-// import {
-//     createBrowserRouter,
-//     Route,
-//     createRoutesFromElements,
-//     RouterProvider,
-//     Link,
-//     Switch
-// } from 'react-router-dom';
-// add and take away some of these
 
 {
   /* <RouterProvider router={Router} /> */
--- src/pages/cuisinePage.js
+++ src/pages/cuisinePage.js
@@ -1,10 +1,54 @@
 import React from 'react';
 
 export default class Cuisine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('Cuisine Selection Submitted: ' + this.state.value);
+    event.preventDefault();
+  }
   render() {
     return (
       <>
-        <h1>Cuisine</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type='text'
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Choose your Cuisine Selection:
+            <select value={this.state.value} onChange={this.handleChange}>
+              <option value=''>
+                Grilled Chicken & Asparagus with Mango Salsa
+              </option>
+              <option value='Fettucini Alfredo with peas & bread'>
+                Fettucini Alfredo with peas & bread
+              </option>
+              <option value='Portobello Mushroom Wellington'>
+                Portobello Mushroom Wellington
+              </option>
+              <option value='Salmon and Cheesy Risotto'>
+                Salmon and Cheesy Risotto
+              </option>
+            </select>
+          </label>
+          <input type='submit' value='Submit' />
+        </form>
+        <h1>Cuisine Selection</h1>
       </>
     );
   }
